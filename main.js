4c85494cae24338f235a8b4f72794161be147313
Alesson <alesson.carlos5@gmail.com>
2023-09-28 14:53:00 -0300

    tabela telefonica

--- main.js
+++ main.js
@@ -1,72 +1,32 @@
-    const form = document.getElementById("form-atividade");
-    const imgAprovado = '<img src="./images/aprovado.png" alt="celebrar" />';
-    const imgReprovado = '<img src="./images/reprovado.png" alt="triste" />';
-    const atividades = [];
-    const notas = [];
-    const notaMinima = parseFloat(prompt("Digite a nota mínima"));
+document.addEventListener('DOMContentLoaded', function () {
+    const contactForm = document.getElementById('contact-form');
+    const contactList = document.getElementById('contact-list');
+    const phoneInput = document.getElementById('phone');
 
-    let linhas = "";
-    const spanAprovado = '<span class="resultado aprovado"></span>';
-    const spanReprovado = '<span class="resultado Reprovado"></span>';
+    contactForm.addEventListener('submit', function (e) {
+        e.preventDefault();
 
-    form.addEventListener("submit", function (e) {
-    e.preventDefault();
+        const name = document.getElementById('name').value;
+        const phone = phoneInput.value;
 
-    adicionarLinha();
-    atualizaTabela();
-    atualizaMediaFinal();
+        // Verifica se o número de telefone contém apenas números
+        if (!/^\d+$/.test(phone)) {
+            alert('Por favor, insira um telefone válido.');
+            return;
+        }
+
+        // Cria uma nova linha na tabela
+        const newRow = document.createElement('tr');
+        newRow.innerHTML = `
+            <td>${name}</td>
+            <td>${phone}</td>
+        `;
+
+        // Adiciona a nova linha à tabela de contatos
+        contactList.appendChild(newRow);
+
+        // Limpa os campos do formulário
+        document.getElementById('name').value = '';
+        phoneInput.value = '';
     });
-
-    function adicionarLinha() {
-    const inputNomeAtividade = document.getElementById("nome-atividade");
-    const inputNotaAtividade = document.getElementById("nota-atividade");
-
-    if (atividades.includes(inputNomeAtividade.value)) {
-        alert(`Atividade: ${inputNomeAtividade.value} já foi inserida`);
-    } else {
-        atividades.push(inputNomeAtividade.value);
-        notas.push(parseFloat(inputNotaAtividade.value));
-
-        let linha = "<tr>";
-        linha += `<td>${inputNomeAtividade.value}</td>`;
-        linha += `<td>${inputNotaAtividade.value}</td>`;
-        linha += `<td>${
-        inputNotaAtividade.value >= notaMinima ? imgAprovado : imgReprovado
-        }</td>`;
-        linha += "</tr>";
-
-        linhas += linha;
-    }
-
-    inputNomeAtividade.value = "";
-    inputNotaAtividade.value = "";
-    }
-
-    function atualizaTabela() {
-    const corpoTabela = document.querySelector("tbody");
-    corpoTabela.innerHTML = linhas;
-    }
-
-    function atualizaMediaFinal() {
-    const mediaFinal = calculaMediaFinal();
-    const resultadoMediaFinal = document.getElementById("resultado-media-final");
-    const mediaFinalValor = document.getElementById("media-final-valor");
-
-    mediaFinalValor.innerHTML = mediaFinal.toFixed(2);
-
-    if (mediaFinal >= notaMinima) {
-        resultadoMediaFinal.innerHTML = imgAprovado;
-    } else {
-        resultadoMediaFinal.innerHTML = "Reprovado";
-    }
-    }
-
-    function calculaMediaFinal() {
-    let somaDasNotas = 0;
-
-    for (let i = 0; i < notas.length; i++) {
-        somaDasNotas += notas[i];
-    }
-
-    return (media = somaDasNotas / notas.length);
-    }
+});
